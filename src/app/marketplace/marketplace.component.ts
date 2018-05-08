8c8a20eb091ecb2b4b24290d7cff7932e89bcccd
Adrian Messado <adrian.messado@gmail.com>
2018-05-08 16:38:26 -0700

    firebase: updating entries

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -14,11 +14,14 @@ import { FirebaseListObservable } from 'angularfire2/database-deprecated';
 export class MarketplaceComponent implements OnInit{
   // albums: Album[] = [];
   albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private albumService: AlbumService) { }
 
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
+    // console.log(this.router.url); --display url route in console
+
   }
 
 
